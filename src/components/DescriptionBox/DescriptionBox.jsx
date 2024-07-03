dd1614af6bd02daa56023b870dd8ac17da11faa0
Tejaswinadendla <saitejaswinadendla045@gmail.com>
2024-07-03 10:01:21 +0530

    Add frontend directory as regular files

---
+++ src/components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './DescriptionBox.css'
+ const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+<div className="descriptionbox-navigator">
+    <div className="descriptionbox-nav-box"> Description</div>
+    <div className="descriptionbox-nav-box.fade"> Reviews(122)</div>
+</div>
+<div className="descriptionbox-description">
+    <p>E-commerce websites are digital platforms that enable businesses to sell products and services to consumers online. These sites feature a catalog of products, each with detailed descriptions, images, and prices. Users can browse, search, and filter products based on various criteria such as category, price range, and brand. The websites offer secure payment gateways to facilitate transactions, accepting various payment methods like credit cards, digital wallets, and bank transfers. They also provide user accounts for personalized experiences, order tracking, and managing preferences. Additionally, e-commerce sites often include features like customer reviews, ratings, and recommendations to enhance shopping decisions. Many also offer promotional deals, discounts, and loyalty programs to attract and retain customers. The backend of these websites integrates inventory management, shipping logistics, and customer service support to ensure smooth operations. Overall, e-commerce websites serve as a comprehensive solution for online retail, combining convenience, variety, and accessibility.</p>
+    <p>  E-commerce sites often include features like customer reviews, ratings, and recommendations to enhance shopping decisions. Many also offer promotional deals, discounts, and loyalty programs to attract and retain customers. </p>
+</div>
+    </div>
+  )
+}
+export default DescriptionBox
